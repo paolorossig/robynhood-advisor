74dd5af66bbba2626a7308f4b7b8245b592c270d
Paolo Rossi <paolo.rossi@pucp.edu.pe>
2023-10-06 14:57:16 -0300

    chore: add prettier-plugin-sort-imports

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,22 @@
-/** @typedef  {import("prettier").Config} PrettierConfig*/
-/** @typedef  {{ tailwindConfig: string }} TailwindConfig*/
+/** @typedef  {import('prettier').Config} PrettierConfig*/
+/** @typedef  {import('prettier-plugin-tailwindcss').PluginOptions} TailwindConfig*/
+/** @typedef  {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig*/
 
-/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+/** @type { PrettierConfig | TailwindConfig | SortImportsConfig} */
 module.exports = {
   semi: false,
   singleQuote: true,
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  importOrder: [
+    '^(next/(.*)$)|^(next$)',
+    '^(react/(.*)$)|^(react$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/(.*)$',
+    '',
+    '^[./]',
+  ],
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import Navbar from '@/components/layout/navbar'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
